b727fc87e802f6b3ecdf8de27f1b3f910f9cd2f8
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2023-11-06 10:31:23 -0500

    When creating system prompt, the document description is now optional (but strongly encouraged)

--- text2table.py
+++ text2table.py
@@ -1,37 +1,23 @@
-sample_gpt_output = """
-# RELEVANCE
-This question is RELEVANT.
+import json
+import openai
+import re
 
-# AVAILABILITY
-The information is STATED in the thing.
-
-# COMPUTATION
-The misspelled or misused words in the letter are as follows:
-1. "borned" instead of "was born"
-2. "Chritsmas" instead of "Christmas" (3 times)
-3. "for buy me something" instead of "to buy me something"
-4. "will born is Chritsmas" instead of "will be born on Christmas"
-5. "swetters" instead of "sweaters"
-6. "friendfull" instead of "friendly"
-
-# DISCUSSION
-The letter contains a total of 8 misspelled or misused words. The word "Chritsmas" is misspelled three times, and each instance is counted separately. The other misspelled words are "borned", "swetters", and "friendfull". The misused phrase is "for buy me something" which should be "to buy me something", and "will born is Chritsmas" which should be "will be born on Christmas".
-
-# ANSWER
-8
-"""
 
 sample_input = """
 Dear Santa Claus, My name is Yadiel and I am 4 years old. I'm from Dominican parents, but I borned in the United States. I wish you to give me something for Chritsmas. My parents do not have enough money for buy me something. My dad is the only one that is working and my mom is pregnant. My sister, Yazlyn, will born is Chritsmas and I will love if you send her something too for Chritsmas. It will mean something big to me if you send her something. My sizes in clothes are the following: coats, t-shirts, swetters: 4t. Pants, pajamas, and interior clothes: 4t. Sneakers, boots and shoes: 11.5. I am a little friendfull (friendly) and loving boy. I've been a good boy this whole year. I got good news for you. I can sleep without doing pee in my bed since June. With Love, Yadiel.
 """
 
 
-def create_systemprompt(question: str, document_description: str):
+def create_systemprompt(question: str, document_description: str = None) -> str:
+    section_docdescr = ""
+    if document_description:
+        section_docdescr = (
+            f"\nThe document can be described as: {document_description}\n"
+        )
+
     systemprompt = f"""
 I will present a short document to you. You will read this document and then extract a single piece of information from that document. You will be graded on your reasoning process and your ability to justify your answer.
-
-The document can be described as: {document_description}
-
+{section_docdescr}
 The piece of information I'd like you to extract is: {question}
 
 I'd like you to present your response in Markdown format, using the following multi-part structure: RELEVANCE, AVAILABILITY, DISCUSSION, and ANSWER. Each part will begin with its header, followed by your content.
@@ -54,11 +40,6 @@ Here, you will state your final answer in a succinct manner, with no other text,
 Good luck.
 """
     return systemprompt
-
-
-import json
-import openai
-import re
 
 
 def split_gpt_output(gpt_output):
